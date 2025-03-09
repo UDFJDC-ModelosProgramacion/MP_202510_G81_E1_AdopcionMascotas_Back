4168212e1d04a18fd1d7fe3a1e1d4e11c61b18a3
estudiante <estudiante@ISIS-2603-306.sis.virtual.uniandes.edu.co>
2025-03-09 10:00:03 -0500

    Add initial files

--- src/main/java/co/edu/udistrital/mdp/adopcion/entities/BaseEntity.java
+++ src/main/java/co/edu/udistrital/mdp/adopcion/entities/BaseEntity.java
@@ -1,4 +1,4 @@
-package co.edu.udistrital.mdp.ZZZ.entities;
+package co.edu.udistrital.mdp.adopcion.entities;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;

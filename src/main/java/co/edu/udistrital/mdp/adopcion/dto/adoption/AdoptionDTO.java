dcff342569acc835c49c496dd03dc242d68f0585
JkVely <jkquinteror@gmail.com>
2025-06-10 10:07:19 -0500

    feat(AdoptionDTO): add imports for OwnerDTO and PetDTO

--- src/main/java/co/edu/udistrital/mdp/adopcion/dto/adoption/AdoptionDTO.java
+++ src/main/java/co/edu/udistrital/mdp/adopcion/dto/adoption/AdoptionDTO.java
@@ -1,7 +1,11 @@
 package co.edu.udistrital.mdp.adopcion.dto.adoption;
+
 import co.edu.udistrital.mdp.adopcion.dto.events.EventDTO;
+import co.edu.udistrital.mdp.adopcion.dto.person.OwnerDTO;
 import co.edu.udistrital.mdp.adopcion.dto.person.VeterinarianDTO;
+import co.edu.udistrital.mdp.adopcion.dto.pet.PetDTO;
 import co.edu.udistrital.mdp.adopcion.entities.adoption.AdoptionStatusEnum;
+
 public class AdoptionDTO extends EventDTO {
     private Long id;
     private String description;
